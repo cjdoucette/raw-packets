64e54af563f47102db1792c98c32f1259e5afe1b
Cody Doucette <doucette@bu.edu>
2020-09-25 20:52:36 +0000

    Capture all packets to port 1234

--- legit/send.sh
+++ legit/send.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-sudo tcpdump -i ens5 '((tcp[tcpflags] == tcp-syn) or (tcp[tcpflags] == tcp-ack)) and port 1234' -n > output.txt &
-#sudo tcpdump -i ens5 'port 1234' -n > output.txt &
+#sudo tcpdump -i ens5 '((tcp[tcpflags] == tcp-syn) or (tcp[tcpflags] == tcp-ack)) and port 1234' -n > output.txt &
+sudo tcpdump -i ens5 'port 1234' -n > output.txt &
 sleep 2
 for i in {1..1000}
 do
