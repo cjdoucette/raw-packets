b8e90a5c3a5181c03391061aa7f8b27c6150f543
Cody Doucette <doucette@bu.edu>
2020-10-06 00:40:18 +0000

    Update legitimate client script

--- legit/send.sh
+++ legit/send.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
-sudo tcpdump -i ens5 'port 1234' -n > legit_log.txt &
+sudo tcpdump -i ens5 -n port 1234 > legit_log.txt &
 sleep 2
-for i in {1..1000}
+for i in {1..100}
 do
-	nc -N -w 1 172.31.3.200 1234 < file.dat
+	if (($i % 100)); then
+		echo $i
+	fi
+	nc -N -w 10 172.31.3.200 1234 < file.dat
 done
 sleep 2
 sudo pkill tcpdump
