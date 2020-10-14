32f15408d51441402f450a021026b5479ef3c400
Cody Doucette <doucette@bu.edu>
2020-10-14 14:19:23 +0000

    Update legitimate client script

--- legit/send.sh
+++ legit/send.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
-sudo tcpdump -i ens5 -n port 1234 > legit_log.txt &
+sudo tcpdump -i ens5 -n src 172.31.0.197 and not port 22 > legit_log.txt &
 sleep 2
 for i in {1..100}
 do
 	if (($i % 100)); then
 		echo $i
 	fi
-	nc -N -w 10 172.31.3.200 1234 < file.dat
+	#nc -N -w 10 172.31.3.200 1234 < file.dat
+	curl --max-time 10 -F 'file=@/home/ubuntu/output.dat' http://172.31.3.200:44444/
 done
 sleep 2
 sudo pkill tcpdump
